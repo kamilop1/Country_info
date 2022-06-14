cb539de63c68d7645842d74ad453e072753fe33b
Kamil <kamilrayman1@gmail.com>
2022-06-14 16:17:15 +0200

    Polish and English search

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 fetch('https://restcountries.com/v3.1/all')
 .then(res => res.json())
 .then(out => readAllCountries(out))
-.catch(err => {alert("load error")})
+.catch(err => {alert("load error: "+err)})
 
-let allCountryNames = [];
+let allCountryNames = [], allCountryPolishNames = [];
 
 function readAllCountries(countries){
-    for(cnt of countries)[
+    for(cnt of countries){
+        allCountryPolishNames.push(cnt.translations.pol.common)
         allCountryNames.push(cnt.name.common)
-    ]
-    allCountryNames.sort()
-    updateDisplay(allCountryNames)
+    }
+    updateDisplay([...allCountryNames,...allCountryPolishNames])
 }
 function updateDisplay(arr){
+    arr.sort()
     let countries = document.querySelector("#countries");
     countries.innerHTML="";
     arr.forEach(el => {
@@ -25,11 +26,10 @@ function updateDisplay(arr){
 let search;
 window.addEventListener("load", evt => {
     search = document.querySelector("#search");
-    console.log(search)
     search.addEventListener("input",evt => {
         let regex = new RegExp(`${search.value}`,"i")
-        console.log(allCountryNames)
-        let filtered = allCountryNames.filter(el => el.match(regex))
-        updateDisplay(filtered)
+        let filtered_eng = allCountryNames.filter(el => el.match(regex))
+        let filtered_pol = allCountryPolishNames.filter(el => el.match(regex))
+        updateDisplay([...filtered_eng,...filtered_pol])
     })
 })
