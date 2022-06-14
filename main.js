3b6ca0fdd7da1e15fd6d07955c44cf56c56a7820
Kamil <kamilrayman1@gmail.com>
2022-06-14 18:39:24 +0200

    Completely reworked, fully functional

--- main.js
+++ main.js
@@ -1,35 +1,36 @@
-fetch('https://restcountries.com/v3.1/all')
-.then(res => res.json())
-.then(out => readAllCountries(out))
-.catch(err => {alert("load error: "+err)})
-
-let allCountryNames = [], allCountryPolishNames = [];
-
-function readAllCountries(countries){
-    for(cnt of countries){
-        allCountryPolishNames.push(cnt.translations.pol.common)
-        allCountryNames.push(cnt.name.common)
+function updateDisplay(fetched_stuff){
+    if(!Array.isArray(fetched_stuff)){
+        document.querySelector("#noResults").style.display="block";
+        document.querySelector("#countries").innerHTML="";
+    } else {
+        document.querySelector("#noResults").style.display="none";
+        let countries = document.querySelector("#countries");
+        countries.innerHTML="";
+        fetched_stuff.sort((a,b) => (a.name.common > b.name.common) ? 1 : ((b.name.common > a.name.common) ? -1 : 0))
+        fetched_stuff.forEach(country => {
+            let link = document.createElement("a");
+            link.setAttribute("class","searchResult");
+            link.setAttribute("href","./country.html?"+country.ccn3);
+            link.innerText=country.name.common;
+            countries.appendChild(link)
+        })
     }
-    updateDisplay([...allCountryNames,...allCountryPolishNames])
 }
-function updateDisplay(arr){
-    arr.sort()
-    let countries = document.querySelector("#countries");
-    countries.innerHTML="";
-    arr.forEach(el => {
-        let div = document.createElement("div");
-        div.setAttribute("class","searchResult");
-        div.innerText=el;
-        countries.appendChild(div)
-    })
+function fetchCountries(search_value){
+    if(search_value == ""){
+        search_value = "all"
+    } else {
+        search_value = "name/"+search_value;
+    }
+    fetch('https://restcountries.com/v3.1/'+search_value)
+    .then(res => res.json())
+    .then(out => updateDisplay(out))
+    .catch(err => {alert("load error: "+err)})
 }
-let search;
 window.addEventListener("load", evt => {
-    search = document.querySelector("#search");
+    let search = document.querySelector("#search");
+    fetchCountries(search.value)
     search.addEventListener("input",evt => {
-        let regex = new RegExp(`${search.value}`,"i")
-        let filtered_eng = allCountryNames.filter(el => el.match(regex))
-        let filtered_pol = allCountryPolishNames.filter(el => el.match(regex))
-        updateDisplay([...filtered_eng,...filtered_pol])
+        fetchCountries(search.value)
     })
 })
